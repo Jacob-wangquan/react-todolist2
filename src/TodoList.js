c700870314cf489f854eb8514696b066be22df35
Jacob-wangquan <510208757@qq.com>
2018-08-08 10:55:53 +0800

    修改

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,24 +1,59 @@
 import React, {Component} from 'react';
-import store from './store';
+import {connect} from 'react-redux';
 
-class TodoList extends Component {
-    constructor(props){
-        super(props);
-        this.state = store.getState();
-    }
-    render() {
-        return (
+// 无状态组件 
+const TodoList = (props) => {
+    const { inputValue, list, changeInputValue, handleClick, handleDelete } = props;
+
+    return (
+        <div>
             <div>
-                <div>
-                    <input value={this.state.inputValue} />
-                    <button>提交</button>
-                </div>
-                <ul>
-                    <li>aaa</li>
-                </ul>
+                <input value={inputValue} onChange={changeInputValue} />
+                <button onClick={handleClick}>提交</button>
             </div>
-        )
+            <ul>
+                {
+                    list.map((item, index) => {
+                        return <li onClick={() => handleDelete(index)} key={index}>{item}</li>
+                    })
+                }
+            </ul>
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => {
+    return {
+        inputValue: state.inputValue,
+        list: state.list
     }
 }
 
-export default TodoList;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        changeInputValue(e) {
+            const action = {
+                type: 'change_input_value',
+                value: e.target.value
+            };
+            dispatch(action);
+        },
+
+        handleClick(){
+            const action = {
+                type: 'add_item'
+            }
+            dispatch(action);
+        },
+
+        handleDelete(index){
+            const action = {
+                type: 'delete_item',
+                index: index
+            };
+            dispatch(action);
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoList from './TodoList';
+import { Provider } from 'react-redux';
+import store from './store';
 
+const App = (
+    <Provider store={store}>
+        <TodoList />
+    </Provider>
+);
 
-ReactDOM.render(<TodoList />, document.getElementById('root'));
+ReactDOM.render(App, document.getElementById('root'));
 
